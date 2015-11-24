b3f57fa287eda905859c60bb2a446fce0e922410
Stuart Urback <stuart@urback.net>
2015-11-24 09:22:01 -0700

    Finished warmup

--- solutions.js
+++ solutions.js
@@ -35,19 +35,28 @@ console.log(people.joe.last_name);
 
 // What are the last names of all the people?
 // HINT: Iterate through the object
-
+for (var individual in people){
+  console.log(people[individual].last_name);
+}
 
 // What is the first genre that each person listed?
 // HINT: Iterate through theo object and console.log each person's first genre choice
+for (var individual in people){
+  console.log(people[individual].preferences.favorite_genres[0]);
+}
 
 
 // What is the last name of the person sitting next to joe?
 // HINT: Start by finding who joe is sitting next to, then find that person in the object
 
+var sittingNextTo = people["joe"].sitting_next_to;
+console.log(people[sittingNextTo].last_name);
 
 // What meal is sue going to eat?
-
+console.log(people["sue"].preferences.meal);
 
 // What was the date of the first correspondence with joe?
+console.log(people["joe"].correspondence[0].date);
 
 // What was the description of the last correspondence with sue?
+console.log(people["sue"].correspondence[people["sue"].correspondence.length - 1].date);
